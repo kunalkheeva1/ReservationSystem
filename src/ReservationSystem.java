2b85a9d4dd7463750b9a1382127ece6b3595b65f
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-09-19 22:32:40 -0700

    added try and catch statement with respect to if the file exist or not, if not then it creates the file else it prints that file already exists. shows error otherwise.

--- src/ReservationSystem.java
+++ src/ReservationSystem.java
@@ -1,5 +1,21 @@
+import java.io.File;
+
 public class ReservationSystem {
     public static void main(String[] args) {
+
+        //file creation function
+        try{
+            File file = new File("file.txt");
+            //if there is no file
+            if(file.createNewFile()){
+                System.out.println("File created Successfully");
+            }
+            else{
+                System.out.println("File already exists! ");
+            }
+        }catch(Exception exception){
+            System.err.println(exception);
+        }
 
     }
     //seat class, passenger class,booking
