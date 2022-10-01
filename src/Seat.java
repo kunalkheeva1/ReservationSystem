346c67b2ff5df0520b6d672c0ed2a523b39358f9
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-09-30 22:35:46 -0700

    added constructor in seat class to find the seat detaisl like row index, seatStatus, serviceClass;

--- src/Seat.java
+++ src/Seat.java
@@ -1,35 +1,18 @@
 public class Seat {
-    int seatRow;
-    char seatLocation;
-    SeatStatus status;
+    int row;
+    int index;
 
-    public Seat(int seatRow, char seaLocation, SeatStatus status) {
-        this.seatRow = seatRow;
-        this.seatLocation = seaLocation;
-        this.status = status;
+    SeatStatus seatStatus;
+    Passenger passenger;
+    ServiceClass serviceClass;
+    public Seat(int row, int index, SeatStatus seatStatus, ServiceClass serviceClass){
+        this.row = row;
+        this.index = index;
+        this.seatStatus = seatStatus;
+        this.serviceClass = serviceClass;
     }
 
-    public int getSeatRow() {
-        return seatRow;
-    }
 
-    public void setSeatRow(int seatRow) {
-        this.seatRow = seatRow;
-    }
 
-    public char getSeatLocation() {
-        return seatLocation;
-    }
 
-    public void setSeatLocation(char seaLocation) {
-        this.seatLocation = seaLocation;
-    }
-
-    public SeatStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(SeatStatus status) {
-        this.status = status;
-    }
 }
