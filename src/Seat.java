5ac56d44121ce6fe9ca5b1fce23f6c490b6185c2
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-09-30 22:36:27 -0700

    added the getters and setters in the seat class to enlighten the details

--- src/Seat.java
+++ src/Seat.java
@@ -12,7 +12,43 @@ public class Seat {
         this.serviceClass = serviceClass;
     }
 
+    public int getRow() {
+        return row;
+    }
 
+    public void setRow(int row) {
+        this.row = row;
+    }
 
+    public int getIndex() {
+        return index;
+    }
 
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public SeatStatus getSeatStatus() {
+        return seatStatus;
+    }
+
+    public void setSeatStatus(SeatStatus seatStatus) {
+        this.seatStatus = seatStatus;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
+    }
+
+    public ServiceClass getServiceClass() {
+        return serviceClass;
+    }
+
+    public void setServiceClass(ServiceClass serviceClass) {
+        this.serviceClass = serviceClass;
+    }
 }
