3533499bc661a36f4e453dd41126ee510583779f
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-09-30 22:24:43 -0700

    added the constructor in resrevation request which is clarifying the request type if single or group and outputting accordingly

--- src/ReservationRequest.java
+++ src/ReservationRequest.java
@@ -1,40 +1,28 @@
+import java.awt.print.Book;
+import java.util.ArrayList;
+
 public class ReservationRequest {
-     ServiceClass userServiceClass;
-     SeatPreference userSeatPreference;
-     Passenger passenger;
+    //arrangement for the passenger
+    BookingType bookingType;
+    ArrayList<Passenger> passengers;
+    SeatPreference seatPreference;
+    ServiceClass serviceClass;
 
-     public ReservationRequest(Passenger passenger, ServiceClass userServiceClass, SeatPreference userSeatPreference) {
-        this.userSeatPreference = userSeatPreference;
-        this.userServiceClass = userServiceClass;
-        this.passenger = passenger;
-    }
+    //groupId
+    String groupId;
 
+    public ReservationRequest(BookingType bookingType, ArrayList<Passenger> passengers, SeatPreference seatPreference, ServiceClass serviceClass, String groupId){
+        this.bookingType = bookingType;
+        this.passengers = passengers;
+        this.serviceClass = serviceClass;
 
-    public SeatPreference getUserSeatPreference() {
-        return userSeatPreference;
-    }
-
-    public void setUserSeatPreference(SeatPreference userSeatPreference) {
-        this.userSeatPreference = userSeatPreference;
-    }
-
-    public ServiceClass getUserServiceClass() {
-        return userServiceClass;
-    }
-
-    public void setUserServiceClass(ServiceClass userServiceClass) {
-        this.userServiceClass = userServiceClass;
+        if(bookingType.equals(BookingType.SINGLE)){
+            this.seatPreference = seatPreference;
+        }else{
+            this.seatPreference = seatPreference;
+            this.groupId = groupId;
+        }
     }
 
 
 }
-//create a new class Booking Service, booking methods, book single , book group, cancel single cancel group,
-
-
-/*if book single send the reservation request to check(),
- book group  pass the list of the reservation request
-
-
- similarly in cancellation, if only one name then create a passenger object and pass to delete only one,
- if group then pass the list to delete it
- */
