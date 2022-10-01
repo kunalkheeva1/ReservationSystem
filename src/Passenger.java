414e4271ddeb1da0730da92647cfd223bce70c58
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-09-30 22:13:49 -0700

    updated the passenger class, as now it will not be holding the details of groups now, it will just hold the details of first and last names of the Passenger

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,22 +1,14 @@
 public class Passenger {
     String firstName, lastName;
-    String groupId;
-
-    public String getGroupId() {
-        return groupId;
-    }
-
-    public void setGroupId(String groupId) {
-        this.groupId = groupId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
 
     public Passenger(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
     }
 
     public void setFirstName(String firstName) {
